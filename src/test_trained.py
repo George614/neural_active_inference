3ac610916cbb840a5700db8315d0732cf68b1a36
George <yangzhizhuo@gmail.com>
2021-05-27 11:14:33 -0400

    Added saving for test simulation as a video

--- src/test_trained.py
+++ src/test_trained.py
@@ -6,27 +6,33 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 logging.getLogger('tensorflow').setLevel(logging.FATAL)
 import tensorflow as tf
 import numpy as np
+import cv2
 import time
 sys.path.insert(0, 'utils/')
 from utils import load_object
 sys.path.insert(0, 'model/')
 from interception_py_env import InterceptionEnv
 
-
-model_save_path = "D:/Projects/neural_active_inference/exp/interception/qai/correct_Rti_mse_normalize_obv/"
-n_episodes = 10
+model_save_path = "D:/Projects/neural_active_inference/exp/interception/qai/correct_Rti_mse_2/"
+n_episodes = 1
 target_speed_idx = 2
 approach_angle_idx = 0
 env = InterceptionEnv(target_speed_idx, approach_angle_idx)
+
 print("Interception environment with target_speed_idx {} and approach_angle_idx {}".format(target_speed_idx, approach_angle_idx))
 frame_duration = 1 / env.FPS
 
-qaiModel = load_object(model_save_path + "qai.agent")
+qaiModel = load_object(model_save_path + "trial4.agent")
 print("Loaded QAI model from {}".format(model_save_path))
 
-for _ in range(n_episodes):
+width = 1000
+height = 407
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+for i in range(n_episodes):
 	observation = env.reset()
-	env.render()
+	img = env.render(mode='rgb_array')
+	video = cv2.VideoWriter(model_save_path+"test_{}.avi".format(i), fourcc, float(env.FPS), (width, height))
 	prev_time = time.time()
 	done = False
 	while not done:
@@ -38,7 +44,10 @@ for _ in range(n_episodes):
 	    observation = next_obv    
 	    time.sleep(max(frame_duration - (time.time() - prev_time), 0))
 	    prev_time = time.time()
-	    env.render()
+	    img = env.render(mode='rgb_array')
+	    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	    video.write(img)
+	video.release()
 
 env.close()
 print("Test simulation finished.")
