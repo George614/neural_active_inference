0c51fdd5597a531a5e879bed0edbf688f3f0d0a7
George <yangzhizhuo@gmail.com>
2021-06-16 21:04:19 -0400

    Added scaling for obv, and check image dims automatically for video

--- src/test_trained.py
+++ src/test_trained.py
@@ -13,28 +13,39 @@ from utils import load_object
 sys.path.insert(0, 'model/')
 from interception_py_env import InterceptionEnv
 
-model_save_path = "D:/Projects/neural_active_inference/exp/interception/qai/correct_-Rti_anneal_Rte_mse/"
+model_save_path = "D:/Projects/neural_active_inference/exp/interception/qai/negRti_pred_obv_anneal_Rte_mse_4D_obv_R35/"
 create_video = True
 n_episodes = 1
 target_speed_idx = 2
-approach_angle_idx = 0
-env = InterceptionEnv(target_speed_idx, approach_angle_idx)
+approach_angle_idx = 3
+use_env_prior = False
+scale_obv = True
+env = InterceptionEnv(target_speed_idx, approach_angle_idx, return_prior=use_env_prior)
 
 print("Interception environment with target_speed_idx {} and approach_angle_idx {}".format(target_speed_idx, approach_angle_idx))
 frame_duration = 1 / env.FPS
 
-trial_num = 1
-episode_num = 500
+trial_num = 2
+episode_num = 50
 qaiModel = load_object(model_save_path + "trial_{}_epd_{}.agent".format(trial_num, episode_num))
 print("Loaded QAI model from {}".format(model_save_path))
 
 if create_video:
-    width = 1000
-    height = 407
-    fourcc = cv2.VideoWriter_fourcc(*'XVID') #or 'mp4v' with '*.mp4' suffix
+    _ = env.reset()
+    env.step(0)
+    img = env.render(mode='rgb_array')
+    width = img.shape[1]
+    height = img.shape[0]
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') # 'XVID' with '.avi' or 'mp4v' with '.mp4' suffix
 
 for i in range(n_episodes):
     observation = env.reset()
+    if scale_obv:
+        # scale obv to range (0, 1)
+        observation[0] /= env.target_init_distance
+        observation[1] /= env.target_max_speed
+        observation[2] /= env.subject_max_position
+        observation[3] /= env.subject_speed_max
     if create_video:
         video = cv2.VideoWriter(model_save_path+"trial_{}_epd_{}.avi".format(trial_num, episode_num), fourcc, float(env.FPS), (width, height))
     prev_time = time.time()
@@ -44,7 +55,16 @@ for i in range(n_episodes):
         obv = tf.expand_dims(obv, axis=0)
         action = qaiModel.act(obv)
         action = action.numpy().squeeze()
-        next_obv, reward, done, _ = env.step(action)
+        if use_env_prior:
+            next_obv, reward, done, prior, _ = env.step(action)
+        else:
+            next_obv, reward, done, _ = env.step(action)
+            if scale_obv:
+                # scale obv to range (0, 1)
+                next_obv[0] /= env.target_init_distance
+                next_obv[1] /= env.target_max_speed
+                next_obv[2] /= env.subject_max_position
+                next_obv[3] /= env.subject_speed_max
         observation = next_obv    
         time.sleep(max(frame_duration - (time.time() - prev_time), 0))
         prev_time = time.time()
