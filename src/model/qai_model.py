88b6d808216a8d79b4c71b4ff41c04b041536d18
George <yangzhizhuo@gmail.com>
2021-07-13 20:56:43 -0400

    added seed to MLPs, removed norm on obvs, positive epistemic, etc...
    tracking of max/min for Rti and Rte are reset,  and signal normalization are reset to the old fashion

--- src/model/qai_model.py
+++ src/model/qai_model.py
@@ -31,12 +31,12 @@ class QAIModel:
         self.use_sum_q = (args.getArg("use_sum_q").strip().lower() == 'true')
         self.instru_term = args.getArg("instru_term")
         self.normalize_signals = (args.getArg("normalize_signals").strip().lower() == 'true')
-        self.normalize_obvs = (args.getArg("normalize_obvs").strip().lower() == 'true')
+        self.seed = int(args.getArg("seed"))
         self.EFE_bound = 1.0
-        self.max_R_ti = -0.01
-        self.min_R_ti = -1.0 #-1.0 for GLL #0.01
-        self.max_R_te = -0.01
-        self.min_R_te = -1.0 #-1.0 for GLL #0.01
+        self.max_R_ti = 1.0
+        self.min_R_ti = 0.01 #-1.0 for GLL #0.01
+        self.max_R_te = 1.0
+        self.min_R_te = 0.01 #-1.0 for GLL #0.01
         self.max_R_t = -0.01
         self.min_R_t = -1.0
         self.obv_clip = 20.0
@@ -64,12 +64,12 @@ class QAIModel:
 
         ## transition model ##
         self.transition = ProbMLP(name="Trans",z_dims=trans_dims, act_fun=act_fun, wght_sd=wght_sd,
-                                  init_type=init_type,use_layer_norm=self.layer_norm)
+                                  init_type=init_type,use_layer_norm=self.layer_norm, seed=self.seed)
         ## EFE value model ##
         self.efe = ProbMLP(name="EFE",z_dims=efe_dims, act_fun=efe_act_fun, wght_sd=wght_sd,
-                           init_type=init_type,use_layer_norm=self.layer_norm)
+                           init_type=init_type,use_layer_norm=self.layer_norm, seed=self.seed)
         self.efe_target = ProbMLP(name="EFE_targ",z_dims=efe_dims, act_fun=efe_act_fun, wght_sd=wght_sd,
-                                  init_type=init_type,use_layer_norm=self.layer_norm)
+                                  init_type=init_type,use_layer_norm=self.layer_norm, seed=self.seed)
 
         # clump all parameter variables of sub-models/modules into one shared pointer list
         self.param_var = []
@@ -86,13 +86,6 @@ class QAIModel:
         self.efe_target.set_weights(self.efe, tau=self.tau)
 
     def act(self, o_t, return_efe=False):
-        if self.normalize_obvs:
-            o_t = tf.clip_by_value(o_t, -self.obv_clip, self.obv_clip)
-            a = -self.obv_bound
-            b = self.obv_bound
-            self.max_obv = tf.math.maximum(self.max_obv, tf.math.reduce_max(o_t))
-            self.min_obv = tf.math.minimum(self.min_obv, tf.math.reduce_min(o_t))
-            o_t = (o_t - self.min_obv) * (b - a) / (self.max_obv - self.min_obv) + a
         if tf.random.uniform(shape=()) > self.epsilon:
             # run EFE model given state at time t
             efe_t, _, _ = self.efe.predict(o_t)
@@ -108,22 +101,6 @@ class QAIModel:
         pass
 
     def train_step(self, obv_t, obv_next, action, done, weights=None, reward=None, obv_prior=None):
-        if self.normalize_obvs:
-            obv_t = tf.clip_by_value(obv_t, -self.obv_clip, self.obv_clip)
-            obv_next = tf.clip_by_value(obv_next, -self.obv_clip, self.obv_clip)
-            a = -self.obv_bound
-            b = self.obv_bound
-            self.max_obv = tf.math.maximum(self.max_obv, tf.math.reduce_max(obv_t))
-            self.max_obv = tf.math.maximum(self.max_obv, tf.math.reduce_max(obv_next))
-            self.min_obv = tf.math.minimum(self.min_obv, tf.math.reduce_min(obv_t))
-            self.min_obv = tf.math.minimum(self.min_obv, tf.math.reduce_min(obv_next))
-            if obv_prior is not None:
-                obv_prior = tf.clip_by_value(obv_prior, -self.obv_clip, self.obv_clip)
-                self.max_obv = tf.math.maximum(self.max_obv, tf.math.reduce_max(obv_prior))
-                self.min_obv = tf.math.minimum(self.min_obv, tf.math.reduce_min(obv_prior))
-                obv_prior = (obv_prior - self.min_obv) * (b - a) / (self.max_obv - self.min_obv) + a
-            obv_t = (obv_t - self.min_obv) * (b - a) / (self.max_obv - self.min_obv) + a
-            obv_next = (obv_next - self.min_obv) * (b - a) / (self.max_obv - self.min_obv) + a
         with tf.GradientTape(persistent=True) as tape:
             ### run s_t and a_t through transition model ###
             o_next_tran_mu, _, _ = self.transition.predict(tf.concat([obv_t, action], axis=-1))
@@ -138,20 +115,20 @@ class QAIModel:
                         o_prior_mu, o_prior_std, _ = self.prior.predict(obv_t)
                         # difference between preferred future and actual future, i.e. instrumental term
                         #R_ti = -1.0 * g_nll(obv_next, o_prior_mu, o_prior_std * o_prior_std, keep_batch=True)
-                        R_ti = -1.0 * mse(x_true=o_next_tran_mu, x_pred=o_prior_mu, keep_batch=True)
+                        R_ti = -1.0 * mse(x_true=obv_next, x_pred=o_prior_mu, keep_batch=True)
                     else:
-                        R_ti = -1.0 * mse(x_true=o_next_tran_mu, x_pred=obv_prior, keep_batch=True)
-                    # if self.normalize_signals is True:
-                    #     a = -self.EFE_bound
-                    #     b = self.EFE_bound
-                    #     self.max_R_ti = max(self.max_R_ti, float(tf.reduce_max(R_ti)))
-                    #     self.min_R_ti = min(self.min_R_ti, float(tf.reduce_min(R_ti)))
-                    #     R_ti = ((R_ti - self.min_R_ti) * (b - a))/(self.max_R_ti - self.min_R_ti) + a
-                    # else:
-                    #     # clip the instrumental value
-                    #     R_ti = tf.clip_by_value(R_ti, -50.0, 50.0)
+                        R_ti = -1.0 * mse(x_true=obv_next, x_pred=obv_prior, keep_batch=True)
+                    if self.normalize_signals is True:
+                        a = -self.EFE_bound
+                        b = self.EFE_bound
+                        self.max_R_ti = max(self.max_R_ti, float(tf.reduce_max(R_ti)))
+                        self.min_R_ti = min(self.min_R_ti, float(tf.reduce_min(R_ti)))
+                        R_ti = ((R_ti - self.min_R_ti) * (b - a))/(self.max_R_ti - self.min_R_ti) + a
+                    else:
+                        # clip the instrumental value
+                        R_ti = tf.clip_by_value(R_ti, -50.0, 50.0)
                 elif self.instru_term == "prior_global":
-                    R_ti = -1.0 * mse(x_true=o_next_tran_mu, x_pred=self.global_mu, keep_batch=True)
+                    R_ti = -1.0 * mse(x_true=obv_next, x_pred=self.global_mu, keep_batch=True)
                     # if self.normalize_signals is True:
                     #     a = -self.EFE_bound
                     #     b = self.EFE_bound
@@ -167,36 +144,34 @@ class QAIModel:
                         R_ti = tf.expand_dims(R_ti, axis=1)
                 ### epistemic term ###
                 delta = (obv_next - o_next_tran_mu)
-                R_te = -1.0 * tf.reduce_sum(delta * delta, axis=1, keepdims=True)
-                # if self.normalize_signals is True:
-                #     a = -self.EFE_bound
-                #     b = self.EFE_bound
-                #     self.max_R_te = max(self.max_R_te, float(tf.reduce_max(R_te)))
-                #     self.min_R_te = min(self.min_R_te, float(tf.reduce_min(R_te)))
-                #     R_te = ((R_te - self.min_R_te) * (b - a))/(self.max_R_te - self.min_R_te) + a
-                # else:
-                #     # clip the epistemic value
-                #     R_te = tf.clip_by_value(R_te, -50.0, 50.0)
-
-                # the nagative EFE value, i.e. the reward. Note the sign here
-                R_t = R_ti + self.rho * R_te
+                R_te = tf.reduce_sum(delta * delta, axis=1, keepdims=True)
                 if self.normalize_signals is True:
                     a = -self.EFE_bound
                     b = self.EFE_bound
-                    self.max_R_t = max(self.max_R_t, float(tf.reduce_max(R_t)))
-                    self.min_R_t = min(self.min_R_t, float(tf.reduce_min(R_t)))
-                    R_t = ((R_t - self.min_R_t) * (b - a)) / (self.max_R_t - self.min_R_t) + a
+                    self.max_R_te = max(self.max_R_te, float(tf.reduce_max(R_te)))
+                    self.min_R_te = min(self.min_R_te, float(tf.reduce_min(R_te)))
+                    R_te = ((R_te - self.min_R_te) * (b - a))/(self.max_R_te - self.min_R_te) + a
+                else:
+                    # clip the epistemic value
+                    R_te = tf.clip_by_value(R_te, -50.0, 50.0)
+
+                # the nagative EFE value, i.e. the reward. Note the sign here
+                R_t = R_ti + self.rho * R_te
+                # if self.normalize_signals is True:
+                #     a = -self.EFE_bound
+                #     b = self.EFE_bound
+                #     self.max_R_t = max(self.max_R_t, float(tf.reduce_max(R_t)))
+                #     self.min_R_t = min(self.min_R_t, float(tf.reduce_min(R_t)))
+                #     R_t = ((R_t - self.min_R_t) * (b - a)) / (self.max_R_t - self.min_R_t) + a
 
             ## model reconstruction loss ##
-            loss_reconst = mse(x_true=obv_next, x_pred=o_next_tran_mu, keep_batch=True) #g_nll(obv_next, o_next_mu, o_next_std * o_next_std)
+            loss_reconst = mse(x_true=obv_next, x_pred=o_next_tran_mu) #, keep_batch=True) #g_nll(obv_next, o_next_mu, o_next_std * o_next_std)
             done = tf.cast(done, dtype=tf.float32)
-            loss_reconst *= (1 - done) # done samples contain invalid observations
-            loss_reconst = tf.math.reduce_mean(loss_reconst)
 
             # regularization for weights
             loss_l2 = 0.0
             if self.l2_reg > 0.0:
-                loss_l2 = (tf.add_n([tf.nn.l2_loss(var) for var in self.param_var if 'W' in var.name])) * self.l2_reg
+                loss_l2 = tf.add_n([tf.nn.l2_loss(var) for var in self.param_var if 'W' in var.name]) * self.l2_reg
             ## compute full loss ##
             loss_model = loss_reconst + loss_l2
             
@@ -218,9 +193,9 @@ class QAIModel:
                 with tape.stop_recording():
                     # EFE values for next state, s_t+1 is from transition model instead of encoder
                     efe_target, _, _ = self.efe_target.predict(obv_next)
-                    efe_new = tf.expand_dims(tf.reduce_max(efe_target, axis=1), axis=1) # max EFE values at t+1
+                    efe_new = tf.reduce_max(efe_target, axis=1, keepdims=True) # max EFE values at t+1
                     y_j = R_t + (efe_new * self.gamma_d) * (1.0 - done)
-                    y_j = (action * y_j) + ( efe_old * (1.0 - action) )
+                    y_j = (action * y_j) + (efe_old * (1.0 - action))
 
             ## Temporal Difference loss ##
             if weights is not None:
@@ -235,7 +210,8 @@ class QAIModel:
                 loss_efe = tf.math.reduce_mean(loss_efe_batch)
             else:
                 if self.efe_loss == "huber":
-                    loss_efe = huber(x_true=y_j, x_pred=efe_old)
+                    # loss_efe = huber(x_true=y_j, x_pred=efe_old)
+                    loss_efe = tf.reduce_mean(tf.losses.huber(y_j, efe_old))
                 else:
                     loss_efe = mse(x_true=y_j, x_pred=efe_old)
 
