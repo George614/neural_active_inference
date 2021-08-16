d93150f124d196e550f967298dffa09ad742caab
George <yangzhizhuo@gmail.com>
2021-08-16 11:48:41 -0400

    minor changes

--- src/run_exp.sh
+++ src/run_exp.sh
@@ -11,4 +11,4 @@ echo $str
 # Write to a temporary file called command.lock, it is just a text file
 echo -e $str > command.lock
 # Create your RC run command. Note the parameters used. If you need less than 16 GB then I suggest using 2XP4 GPUs.
-sbatch -J ${baseJobName} -o "RC_generated_log_file.o" -e "RC_generated_error_file.e" --mem=8G --cpus-per-task=4 -p tier3 -A actinf --gres=gpu:p4:1 -t 1-0:0:0 command.lock
+sbatch -J ${baseJobName} -o "RC_generated_log_file.o" -e "RC_generated_error_file.e" --mem=16G --cpus-per-task=4 -p tier3 -A actinf --gres=gpu:p4:1 -t 1-0:0:0 --mail-user=zy8981@rit.edu --mail-type=ALL command.lock
