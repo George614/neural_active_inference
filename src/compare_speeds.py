b635183b93b740abfb737f43e01c2f868c4152ea
George <yangzhizhuo@gmail.com>
2021-09-19 20:20:08 -0400

    Added automation to generate N plots for all initial speeds

--- src/compare_speeds.py
+++ src/compare_speeds.py
@@ -22,10 +22,6 @@ import matplotlib.pyplot as plt
 sys.path.insert(0, 'model/')
 from interception_py_env import InterceptionEnv
 
-approach_angle_idx = 3
-num_trials = 1
-env_prior = 'prior_error' # or prior_obv or None
-
 
 def load_AIF_agent(trial_num, episode_num, model_save_path):
     qaiModel = load_object(model_save_path + "trial_{}_epd_{}.agent".format(trial_num, episode_num))
@@ -68,46 +64,54 @@ if __name__ == "__main__":
     gpu_devices = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpu_devices:
         tf.config.experimental.set_memory_growth(gpu, True)
-    target_speed_idx = 2
-    env = InterceptionEnv(target_speed_idx, approach_angle_idx, return_prior=env_prior, perfect_prior=True)
-    print("Interception environment with target_speed_idx {} and approach_angle_idx {}".format(target_speed_idx, approach_angle_idx))
-    env2 = copy.deepcopy(env)
-    # run the AIF agent on the task with different target initial speeds
+    # set testing conditions
+    approach_angle_idx = 3
+    env_prior = 'prior_error' # or prior_obv or None
+    num_trials = 10
+    
+    # load the AIF agents trained with all target initial speeds
     model_save_path = "D:/Projects/neural_active_inference/exp/interception/qai/negRti_posRte_mse_4D_obv_w_speedchg_allSpeeds_PriorError_DQNhyperP_512net_noL2Reg_relu_learnSche_3k/"
-    qaiModel = load_AIF_agent(0, 2950, model_save_path)
-    agent1_speeds, required_speeds1, first_order_speeds1, succeed1, dist_list1 = record_speeds(target_speed_idx, env, qaiModel=qaiModel)
+    qaiModel1 = load_AIF_agent(0, 2950, model_save_path)
     model_save_path = "D:/Projects/neural_active_inference/exp/interception/qai/negRti_posRte_mse_4D_obv_w_speedchg_allSpeeds_PriorError_realtimePrior_DQNhyperP_512net_noL2Reg_relu_learnSche_3k/"
-    qaiModel = load_AIF_agent(0, 2950, model_save_path)
-    agent2_speeds, required_speeds2, first_order_speeds2, succeed2, dist_list2 = record_speeds(target_speed_idx, env2, qaiModel=qaiModel)
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 4))
-    # plot speeds for different agents and different priors    
-    ax1.plot(np.arange(len(agent1_speeds)), agent1_speeds, color='red', label='perfect_agent', linewidth=0.5)
-    ax1.plot(np.arange(len(required_speeds1)), required_speeds1, color='orange', label='perfect_speed1', linewidth=0.5)
-    ax1.plot(np.arange(len(first_order_speeds1)), first_order_speeds1, linestyle = '--', color='orange', label='1st-order_speed1', linewidth=0.5)
-    ax1.plot(np.arange(len(agent2_speeds)), agent2_speeds, color='blue', label='1st-order_agent', linewidth=0.5)
-    ax1.plot(np.arange(len(required_speeds2)), required_speeds2, color='cyan', label='perfect_speed2', linewidth=0.5)
-    ax1.plot(np.arange(len(first_order_speeds2)), first_order_speeds2, linestyle = '--', color='cyan', label='1st-order_speed2', linewidth=0.5)
-    ax1.axhline(y=max(env.action_speed_mappings), color = 'black', linestyle = '-', label='max_speed', linewidth=0.5)
-    ax1.legend(bbox_to_anchor=(0., 1.1, 1., .2), loc='lower left', fontsize='xx-small', ncol=2, mode='expand', borderaxespad=0.)
-    ax1.set_ylim([0, 15])
-    ax1.set_ylabel("Speed")
-    ax1.set_xlabel("Time steps")
-    ax1.text(0, 13, "perfect_agent {}".format("succeed" if succeed1 else "failed", fontsize=5))
-    ax1.text(0, 12, "1st-order_agent {}".format("succeed" if succeed2 else "failed", fontsize=5))
-    ax1.set_title("Speed comparison")
-    # plot connection lines between subject and target throughout an episode
-    for dist_pair in dist_list1:
-        ax2.plot([dist_pair[1], 0], [0, dist_pair[0]], 'go-', linewidth=2)
-    ax2.set_xlim([0, env.subject_init_distance_max])
-    ax2.set_ylim([0, env.target_init_distance])
-    ax2.set_xlabel("Subject distance")
-    ax2.set_ylabel("Target distance")
-    ax2.set_title("Perfect agent")
-    for dist_pair in dist_list2:
-        ax3.plot([dist_pair[1], 0], [0, dist_pair[0]], 'bo-', linewidth=2)
-    ax3.set_xlim([0, env.subject_init_distance_max])
-    ax3.set_ylim([0, env.target_init_distance])
-    ax3.set_xlabel("Subject distance")
-    ax3.set_ylabel("Target distance")
-    ax3.set_title("1st-order_agent")
-    fig.savefig(os.getcwd() + "/speeds_compare_speedIdx_{}_{}.png".format(target_speed_idx, time.time_ns()//int(1e9)), dpi=200, bbox_inches="tight")
+    qaiModel2 = load_AIF_agent(0, 2950, model_save_path)
+    
+    for target_speed_idx in [0, 1, 2]:
+        env = InterceptionEnv(target_speed_idx, approach_angle_idx, return_prior=env_prior, perfect_prior=True)
+        print("Interception environment with target_speed_idx {} and approach_angle_idx {}".format(target_speed_idx, approach_angle_idx))
+        env2 = copy.deepcopy(env)
+
+        for trial in range(num_trials):
+            agent1_speeds, required_speeds1, first_order_speeds1, succeed1, dist_list1 = record_speeds(target_speed_idx, env, qaiModel=qaiModel1)
+            agent2_speeds, required_speeds2, first_order_speeds2, succeed2, dist_list2 = record_speeds(target_speed_idx, env2, qaiModel=qaiModel2)
+            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 4))
+            # plot speeds for different agents and different priors    
+            ax1.plot(np.arange(len(agent1_speeds)), agent1_speeds, color='red', label='perfect_agent', linewidth=0.5)
+            ax1.plot(np.arange(len(required_speeds1)), required_speeds1, color='orange', label='perfect_speed1', linewidth=0.5)
+            ax1.plot(np.arange(len(first_order_speeds1)), first_order_speeds1, linestyle = '--', color='orange', label='1st-order_speed1', linewidth=0.5)
+            ax1.plot(np.arange(len(agent2_speeds)), agent2_speeds, color='blue', label='1st-order_agent', linewidth=0.5)
+            ax1.plot(np.arange(len(required_speeds2)), required_speeds2, color='cyan', label='perfect_speed2', linewidth=0.5)
+            ax1.plot(np.arange(len(first_order_speeds2)), first_order_speeds2, linestyle = '--', color='cyan', label='1st-order_speed2', linewidth=0.5)
+            ax1.axhline(y=max(env.action_speed_mappings), color = 'black', linestyle = '-', label='max_speed', linewidth=0.5)
+            ax1.legend(bbox_to_anchor=(0., 1.1, 1., .2), loc='lower left', fontsize='xx-small', ncol=2, mode='expand', borderaxespad=0.)
+            ax1.set_ylim([0, 15])
+            ax1.set_ylabel("Speed")
+            ax1.set_xlabel("Time steps")
+            ax1.text(0, 13, "perfect_agent {}".format("succeed" if succeed1 else "failed", fontsize=5))
+            ax1.text(0, 12, "1st-order_agent {}".format("succeed" if succeed2 else "failed", fontsize=5))
+            ax1.set_title("Speed comparison")
+            # plot connection lines between subject and target throughout an episode
+            for dist_pair in dist_list1:
+                ax2.plot([dist_pair[1], 0], [0, dist_pair[0]], 'go-', linewidth=2)
+            ax2.set_xlim([0, env.subject_init_distance_max])
+            ax2.set_ylim([0, env.target_init_distance])
+            ax2.set_xlabel("Subject distance")
+            ax2.set_ylabel("Target distance")
+            ax2.set_title("Perfect agent")
+            for dist_pair in dist_list2:
+                ax3.plot([dist_pair[1], 0], [0, dist_pair[0]], 'bo-', linewidth=2)
+            ax3.set_xlim([0, env.subject_init_distance_max])
+            ax3.set_ylim([0, env.target_init_distance])
+            ax3.set_xlabel("Subject distance")
+            ax3.set_ylabel("Target distance")
+            ax3.set_title("1st-order_agent")
+            fig.savefig(os.getcwd() + "/plotting/speeds_compare_speedIdx_{}_trial_{}.png".format(target_speed_idx, trial+1), dpi=200, bbox_inches="tight")
